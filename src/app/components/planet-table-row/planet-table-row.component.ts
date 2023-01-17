20becf34dcc3b80413d7026db736a006dc42a6bd
EvgenII Lysenko <genja1988@inbox.ru>
2023-01-17 09:48:56 +0300

    added table with planet info

--- src/app/components/planet-table-row/planet-table-row.component.ts
+++ src/app/components/planet-table-row/planet-table-row.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { IPlanetData } from 'src/app/models/models';
 
 @Component({
   selector: 'app-planet-table-row',
   templateUrl: './planet-table-row.component.html',
-  styleUrls: ['./planet-table-row.component.scss']
+  styleUrls: ['./planet-table-row.component.scss'],
 })
 export class PlanetTableRowComponent implements OnInit {
+  @Input() planetInfo: IPlanetData | null = null;
+  @Input() index: number = 0;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
