0f97284efbfc86b8b438d9f23f0acb255c9c9a28
EvgenII Lysenko <genja1988@inbox.ru>
2023-01-17 17:26:00 +0300

    added planet filter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { PlanetTableComponent } from './components/planet-table/planet-table.component';
 import { PlanetFilterComponent } from './components/planet-filter/planet-filter.component';
 import { PlanetTableRowComponent } from './components/planet-table-row/planet-table-row.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, PlanetTableComponent, PlanetFilterComponent, PlanetTableRowComponent],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    PlanetTableComponent,
+    PlanetFilterComponent,
+    PlanetTableRowComponent,
+  ],
+  imports: [BrowserModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/planet-filter/planet-filter.component.ts
+++ src/app/components/planet-filter/planet-filter.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-planet-filter',
   templateUrl: './planet-filter.component.html',
-  styleUrls: ['./planet-filter.component.scss']
+  styleUrls: ['./planet-filter.component.scss'],
 })
-export class PlanetFilterComponent implements OnInit {
+export class PlanetFilterComponent implements OnInit, OnDestroy {
+  filterForm: FormGroup = new FormGroup({
+    filterNameControl: new FormControl(),
+    filterPopulationFromControl: new FormControl('', [Validators.min(0)]),
+    filterPopulationToControl: new FormControl('', [Validators.min(0)]),
+  });
+  destroySub: Subject<boolean> = new Subject();
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.filterForm.valueChanges
+      .pipe(takeUntil(this.destroySub))
+      .subscribe((changes) => {
+        console.log(this.filterForm.controls);
+        Object.keys(this.filterForm.controls).forEach((control) => {
+          this.filterForm.controls[control].updateValueAndValidity({
+            onlySelf: true,
+          });
+          this.filterForm.controls[control].markAsTouched();
+        });
+      });
   }
 
+  ngOnDestroy(): void {
+    this.destroySub.next(true);
+  }
+
+  onSubmit() {
+    console.log(this.filterForm.getRawValue());
+  }
 }
